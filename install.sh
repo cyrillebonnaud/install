5b4631040601e10cced8f6f65fbd8e0a3d30ae2e
cyrillebonnaud <bonnaudc@gmail.com>
2017-03-20 12:01:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # add repo
-sudo apt-add-repository ppa:git-core/ppa -y
+sudo add-apt-repository ppa:git-core/ppa -y
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
 sudo apt-get update
